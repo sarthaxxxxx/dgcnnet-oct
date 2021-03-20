3fb7c14862b99541e4c68f066e15b9d1ea166943
Sarthak Maharana <sarthakmaharana9811@gmail.com>
2021-03-21 01:25:58 +0530

    changed bottleneck dir

--- src/models/DGCNNet.py
+++ src/models/DGCNNet.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from bottleneck import Bottleneck as bot
+from src.models.bottleneck import Bottleneck as bot
 
 
 def DGCNNet_ResNet101(classes):
